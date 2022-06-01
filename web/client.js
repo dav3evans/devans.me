e0cac71d624526c91ed71734a653b6eda2d9b856
David Evans <david.evans@citizensadvice.org.uk>
2022-06-01 13:21:27 +0100

    Added api version date to stop deprecation warning in console.

--- web/client.js
+++ web/client.js
@@ -3,6 +3,7 @@ import sanityClient from '@sanity/client'
 
 export default sanityClient({
   projectId: 'mfgmm5gx', // you can find this in sanity.json
+  apiVersion: '2022-06-01',
   dataset: 'production', // or the name you chose in step 1
   useCdn: true // `false` if you want to ensure fresh data
 })
